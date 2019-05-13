282b215f487219f7956934ed51b420e3a32df32b
gabriele <gabriele.bassi@yahoo.it>
2019-05-13 13:45:55 +0200

    Updated Ticket entity added updated_at and created_at fields

--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -32,6 +32,16 @@ class Ticket
      */
     private $owner;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_at;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -69,6 +79,30 @@ class Ticket
     public function setOwner(?User $owner): self
     {
         $this->owner = $owner;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
 
         return $this;
     }
